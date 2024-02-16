7a32fb714880da74894d3d1c362a523e88baa9ff
Kevanavellaneda <l19141120@queretaro.tecnm.mx>
2024-02-16 12:08:41 -0600

    CardView modifications

--- app/src/main/java/com/example/cadeapp/cardVinedos.java
+++ app/src/main/java/com/example/cadeapp/cardVinedos.java
@@ -2,17 +2,40 @@ package com.example.cadeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
 import com.example.cadeapp.R;
 
 public class cardVinedos extends AppCompatActivity {
+
+    ImageView flecha2;
+
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_vinedos);
+
+        flecha2 = findViewById(R.id.flechaatras2);
+
+        flecha2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(cardVinedos.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
     public void onBackPressed(){
