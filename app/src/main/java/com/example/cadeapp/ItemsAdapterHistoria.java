e0c130ef73457b8edd95c6efcb65af89c3452e60
Kevanavellaneda <l19141120@queretaro.tecnm.mx>
2024-02-19 10:52:39 -0600

    Adapter and viewholder adding

---
+++ app/src/main/java/com/example/cadeapp/ItemsAdapterHistoria.java
@@ -0,0 +1,54 @@
+package com.example.cadeapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsAdapterHistoria extends RecyclerView.Adapter<ItemsAdapterHistoria.ViewHolder> {
+
+    private List<String> items;
+    private Context context;
+
+    public ItemsAdapterHistoria(List<String> items, Context context){
+        this.items = items;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemsAdapterHistoria.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(context).inflate(R.layout.item_viewholdercardviews,parent,false);
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //ItemsDomainHistoria itemsDomainHistoria = items.get(position);
+        Glide.with(context).load(items.get(position)).into(holder.pic);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView pic;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            pic=itemView.findViewById(R.id.url);
+        }
+    }
+}
--- app/src/main/java/com/example/cadeapp/cardVinedos.java
+++ app/src/main/java/com/example/cadeapp/cardVinedos.java
@@ -2,23 +2,40 @@ package com.example.cadeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.example.cadeapp.R;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class cardVinedos extends AppCompatActivity {
 
     ImageView flecha2;
 
-    private RequestQueue mRequestQueue;
-    private StringRequest mStringRequest;
-    private ProgressBar progressBar;
+    private RecyclerView imagesRecycler1;
+    private ItemsAdapterHistoria itemsAdapterHistoria;
+    private List<String> items;
+    private FirebaseFirestore mFirestore;
+
+    ProgressBar pbProgressMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +53,40 @@ public class cardVinedos extends AppCompatActivity {
             }
         });
 
+
+        imagesRecycler1 = findViewById(R.id.imagesRecycler);
+        imagesRecycler1.setHasFixedSize(true);
+        imagesRecycler1.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+
+        mFirestore = FirebaseFirestore.getInstance();
+        items = new ArrayList<>();
+        itemsAdapterHistoria = new ItemsAdapterHistoria(items, this);
+        imagesRecycler1.setAdapter(itemsAdapterHistoria);
+
+        cargarImagenesDesdeFirestore();
+
     }
+
+    private void cargarImagenesDesdeFirestore() {
+        mFirestore.collection("imagesall")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Obtener la URL de la imagen de cada documento y agregarla a la lista
+                            String imageUrl = document.getString("url");
+                            items.add(imageUrl);
+                        }
+                        // Notificar al adaptador sobre el cambio en los datos
+                        itemsAdapterHistoria.notifyDataSetChanged();
+                    }
+                    else {
+                    }
+                });
+    }
+
+
 
     public void onBackPressed(){
         super.onBackPressed();
