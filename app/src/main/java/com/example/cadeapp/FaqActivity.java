06ffa642d38d606ecb679e05982c81a4c3dde9d9
Christian Rios <crismancilla36@gmail.com>
2024-03-01 09:39:39 -0600

    FAQ data edited

--- app/src/main/java/com/example/cadeapp/FaqActivity.java
+++ app/src/main/java/com/example/cadeapp/FaqActivity.java
@@ -48,10 +48,10 @@ public class FaqActivity extends ScrollingActivity {
         versionsList.add(new Versions("¿Cómo puedo encontrar eventos en mi zona?", "Para encontrar eventos en tu zona, puedes utilizar la función de búsqueda de la app. \n" +
                 "También  puedes utilizar el mapa de la app para ver dónde se encuentran las catas que están cerca de ti. \n" +
                 "O si lo prefieres, puedes utilizar los filtros de la app para buscar eventos específicos."));
-        versionsList.add(new Versions("¿Cómo puedo reservar un lugar en un evento de cata?", "Para reservar un lugar en un evento de cata, simplemente haz clic en el evento que te interesa y luego haz clic en el botón \"Reservar\". La app te llevará a una página donde podrás introducir tus datos personales y completar el pago."));
-        versionsList.add(new Versions("¿Cuánto cuestan las catas?", "El precio de las catas varía según el tipo de cata, la ubicación y el lugar que organice el evento."));
-        versionsList.add(new Versions("¿Cómo puedo pagar mi reservación?", "Puedes pagar tu reservación con tarjeta de crédito, débito o PayPal. El pago se realiza a través de la app de forma segura y sencilla."));
-        versionsList.add(new Versions("¿Cómo puedo cancelar o modificar mi reserva?", "Si necesitas cancelar o modificar tu reserva, puedes hacerlo a través de la app. Simplemente haz clic en el botón \"Cancelar\" o \"Modificar\" que aparece junto a la reserva que deseas modificar."));
+        versionsList.add(new Versions("¿Cómo puedo encontrar negocios de barbacoa en mi zona?", "Para reservar un lugar en un evento de cata, simplemente haz clic en el evento que te interesa y luego haz clic en el botón \"Reservar\". La app te llevará a una página donde podrás introducir tus datos personales y completar el pago."));
+        versionsList.add(new Versions("¿Qué puedo encontar en la Ruta de barbacoa y el pulque?", "El precio de las catas varía según el tipo de cata, la ubicación y el lugar que organice el evento."));
+        versionsList.add(new Versions("¿Es necesario hacer una reservación?", "Puedes pagar tu reservación con tarjeta de crédito, débito o PayPal. El pago se realiza a través de la app de forma segura y sencilla."));
+        versionsList.add(new Versions("¿Cómo puedo dar de alta mi negocio de barbacoa?", "Si necesitas cancelar o modificar tu reserva, puedes hacerlo a través de la app. Simplemente haz clic en el botón \"Cancelar\" o \"Modificar\" que aparece junto a la reserva que deseas modificar."));
 
     }
 
