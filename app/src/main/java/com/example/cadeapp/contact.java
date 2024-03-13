569f740211da042e15099a94d15d258e12e40853
Kevanavellaneda <l19141120@queretaro.tecnm.mx>
2024-03-13 14:27:47 -0600

    frontend for settings and profile screens

--- app/src/main/java/com/example/cadeapp/contact.java
+++ app/src/main/java/com/example/cadeapp/contact.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
@@ -15,7 +18,19 @@ public class contact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        // Cambiar el color de la barra de estado
+        cambiarColorBarraEstado(getResources().getColor(R.color.black));
 
+    }
+
+    private void cambiarColorBarraEstado(int color) {
+        // Comprobar la versión de Android
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            // Configurar el color de la barra de estado
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(color);
+        }
     }
 
     public void onBackPressed() {
