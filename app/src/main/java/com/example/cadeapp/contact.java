e2e082481ff99ec7d9b827e376031d2b95d69786
Christian Rios <crismancilla36@gmail.com>
2024-03-22 11:51:17 -0600

    Social Media and contact buttons handling

--- app/src/main/java/com/example/cadeapp/contact.java
+++ app/src/main/java/com/example/cadeapp/contact.java
@@ -1,10 +1,17 @@
 package com.example.cadeapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import static com.google.android.material.internal.ContextUtils.getActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,7 +24,6 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -28,6 +34,7 @@ public class contact extends AppCompatActivity {
     protected ImageView facebook_im, x_im, instagram_im;
     protected LinearLayout social_media_list;
     protected int social_media_weight;
+    protected String facebook_link, x_twitter_link, instagram_link;
 
 
     @Override
@@ -48,6 +55,7 @@ public class contact extends AppCompatActivity {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference docRef = db.collection("soporte").document("contacto");
 
+
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -57,31 +65,59 @@ public class contact extends AppCompatActivity {
                         // Extract support contact
                         String phone = document.getString("telefono");
                         String email = document.getString("email");
-                        String facebook = document.getString("facebook");
-                        String x_twitter = document.getString("x");
-                        String instagram = document.getString("instagram");
+                        facebook_link = document.getString("facebook");
+                        x_twitter_link = document.getString("x");
+                        instagram_link = document.getString("instagram_link");
 
                         // Show on layout
                         phone_text.setText(phone);
                         email_text.setText(email);
 
+
+                        // OnClickListener for facebook button
+                        facebook_im.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse(facebook_link));
+                                startActivity(intent);
+                            }
+                        });
+                        // OnClickListener for facebook button
+                        x_im.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse(x_twitter_link));
+                                startActivity(intent);
+                            }
+                        });
+                        // OnClickListener for facebook button
+                        instagram_im.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse(instagram_link));
+                                startActivity(intent);
+                            }
+                        });
+
                         // Social media
                         social_media_weight = 0;
-                        if (facebook != null && !facebook.isEmpty()) {
+                        if (facebook_link != null && !facebook_link.isEmpty()) {
                             social_media_weight++;
                             facebook_im.setVisibility(View.VISIBLE);
                         }
-                        if (x_twitter != null && !x_twitter.isEmpty()) {
+                        if (x_twitter_link != null && !x_twitter_link.isEmpty()) {
                             social_media_weight++;
                             x_im.setVisibility(View.VISIBLE);
                         }
-                        if (instagram != null && !instagram.isEmpty()) {
+                        if (instagram_link != null && !instagram_link.isEmpty()) {
                             social_media_weight++;
                             instagram_im.setVisibility(View.VISIBLE);
                         }
                         if(social_media_weight > 0)
                             social_m_legend.setVisibility(View.VISIBLE);
-                        social_media_list.setWeightSum(social_media_weight);
+
+                        // Set Weight of each element on social media
+                        // social_media_list.setWeightSum(social_media_weight);
 
                     } else {
                         Log.d("Firestore", "No se han podido cargar los datos de contacto");
