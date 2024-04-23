214dff1cc1dca9aff2563c17cdb13dde64b8e952
Christian Rios <crismancilla36@gmail.com>
2024-04-23 14:10:06 -0600

    Change background for today cell

--- app/src/main/java/com/example/cadeapp/MainActivity.java
+++ app/src/main/java/com/example/cadeapp/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.text.LineBreaker;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -699,7 +700,9 @@ public class MainActivity extends AppCompatActivity {
             boolean tieneEvento = tieneEventoEnFecha(date);
             if (tieneEvento) {
                 // Cambiamos el color de fondo de la celda si tiene un evento
-                cellView.setBackgroundColor(Color.YELLOW); // Ponemos de color la celda
+                cellView.setBackgroundColor(Color.rgb(255, 165, 0)); // Ponemos de color la celda
+            } else if (isToday(date)){
+                cellView.setBackgroundColor(Color.rgb(178, 218, 250)); // Ponemos de color la celda
             } else {
                 cellView.setBackgroundColor(getResources().getColor(R.color.white));
             }
@@ -735,6 +738,17 @@ public class MainActivity extends AppCompatActivity {
             }
             // Si no hay eventos en la fecha dada, devuelve false
             return false;
+        }
+
+        // Verifica si es la fecha de hoy
+        private boolean isToday(Date date){
+            Calendar cal1 = Calendar.getInstance();
+            Calendar cal2 = Calendar.getInstance();
+            cal1.setTime(date);
+            cal2.setTime(today);
+            return cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR) &&
+                    cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR);
+
         }
     }
 
